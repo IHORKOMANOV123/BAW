ea1e0ae461bdff348d039c8fcf1fe65a0d7504d9
IHORKOMANOV123 <ihorkomanov@gmail.com>
2025-03-24 20:41:38 +0100

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
-    header("Location: inna_chroniona.php");
-} else {
-    header("Location: login_view.php");
-}
-exit();
